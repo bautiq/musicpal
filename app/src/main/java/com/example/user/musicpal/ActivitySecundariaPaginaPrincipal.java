5baf9243a7b794ea4541878bc42638a64050c658
Dani <hugodanielmora@gmail.com>
2018-05-16 22:28:43 -0300

    quintoCommit
    intento de segunda pantalla, detecte error en pantalla principal con respecto al fragment, no pude solucionarlo

--- app/src/main/java/com/example/user/musicpal/ActivitySecundariaPaginaPrincipal.java
+++ app/src/main/java/com/example/user/musicpal/ActivitySecundariaPaginaPrincipal.java
@@ -6,19 +6,19 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivitySecundariaPaginaPrincipal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        cargarFragment(new fragmentPantallaInicio());
+        setContentView(R.layout.activity_secundaria_pagina_principal);
+        cargarFragment(new FragmentClickEnPaginaPrincipal());
     }
 
     public void cargarFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.container_fragment, fragment);
+        fragmentTransaction.add(R.id.contenedor_fragment_vista_previa, fragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
 
--- app/src/main/java/com/example/user/musicpal/FragmentClickEnPaginaPrincipal.java
+++ app/src/main/java/com/example/user/musicpal/FragmentClickEnPaginaPrincipal.java
@@ -11,13 +11,13 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class fragmentPantallaInicio extends Fragment {
+public class FragmentClickEnPaginaPrincipal extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_fragment_pantalla_inicio, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment_click_en_pagina_principal, container, false);
         return view;
     }
 
