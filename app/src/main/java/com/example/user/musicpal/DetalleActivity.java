ab784780e416fef1d427606a4d87db3e16f0da2e
bauti <bautiquere@gmail.com>
2018-05-30 13:19:41 -0300

    deshice los cambios

--- app/src/main/java/com/example/user/musicpal/DetalleActivity.java
+++ app/src/main/java/com/example/user/musicpal/DetalleActivity.java
@@ -9,20 +9,18 @@ import android.os.Bundle;
 
 public class DetalleActivity extends AppCompatActivity {
 
-//private DetalleFragment detalleFragment; probe sacar los atributos y no se necesitan por lo q veo..ok?
-//private FragmentManager fragmentManager;
-
+private DetalleFragment detalleFragment;
+private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle);
-
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        DetalleFragment detalleFragment = new DetalleFragment();
+        detalleFragment = new DetalleFragment();
         detalleFragment.setArguments(bundle);
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager = getSupportFragmentManager();
         FragmentHelper.cargarFragmemt(detalleFragment, R.id.contenedor_fragment_vista_previa, fragmentManager);
     }
 
