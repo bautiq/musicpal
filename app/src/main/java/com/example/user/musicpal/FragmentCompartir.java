9a8adbc84ce1ca506267b5c9fc2ac9ac1077f8b4
Dani <hugodanielmora@gmail.com>
2018-06-04 00:21:11 -0300

    compartir con recycler

--- app/src/main/java/com/example/user/musicpal/FragmentCompartir.java
+++ app/src/main/java/com/example/user/musicpal/FragmentCompartir.java
@@ -3,9 +3,15 @@ package com.example.user.musicpal;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,13 +19,34 @@ import android.view.ViewGroup;
  */
 public class FragmentCompartir extends Fragment {
 
+    private RecyclerView recyclerViewRedSocial;
+    private RedSocialAdapter redSocialAdapter;
+    private List<RedSocial> redesSociales;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_fragment_compartir, container, false);
+        recyclerViewRedSocial = view.findViewById(R.id.recycler_red_social_id);
+
+        armarListadoRedes();
+
+        redSocialAdapter = new RedSocialAdapter(redesSociales);
+        recyclerViewRedSocial.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL,false));
+        recyclerViewRedSocial.setHasFixedSize(true);
+        recyclerViewRedSocial.setAdapter(redSocialAdapter);
+
         return view;
+    }
+
+    private void armarListadoRedes() {
+        redesSociales = new ArrayList<>();
+        redesSociales.add(new RedSocial("Facebook", R.drawable.logof));
+        redesSociales.add(new RedSocial("Instagram", R.drawable.logoinst));
+        redesSociales.add(new RedSocial("Twitter", R.drawable.logotwi));
+        redesSociales.add(new RedSocial("WhatsApp", R.drawable.logowhat));
+        redesSociales.add(new RedSocial("Snapchat", R.drawable.logosnap));
     }
 
 }
---
+++ app/src/main/java/com/example/user/musicpal/RedSocialAdapter.java
@@ -0,0 +1,68 @@
+package com.example.user.musicpal;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RedSocialAdapter extends RecyclerView.Adapter {
+
+    private List<RedSocial> redesSociales;
+
+    public RedSocialAdapter(List<RedSocial> redesSociales) {
+        this.redesSociales = redesSociales;
+    }
+
+    public void setRedesSociales(List<RedSocial> redesSociales) {
+        this.redesSociales = redesSociales;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.celda_recycler_red_social, parent, false);
+        RedSocialAdapter.ViewHolderRedSocial viewHolderRedSocial = new ViewHolderRedSocial(view);
+        return viewHolderRedSocial;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        RedSocial redSocial = redesSociales.get(position);
+        ViewHolderRedSocial viewHolderRedSocial = (ViewHolderRedSocial) holder;
+        viewHolderRedSocial.cargarRedSocial(redSocial);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (redesSociales != null) {
+            return redesSociales.size();
+        } else {
+            return 0;
+        }
+    }
+
+    public class ViewHolderRedSocial extends RecyclerView.ViewHolder {
+
+        private TextView textViewNombre;
+        private ImageView imagenRed;
+
+        public ViewHolderRedSocial(View itemView) {
+            super(itemView);
+            this.textViewNombre = itemView.findViewById(R.id.nombre_red_social_id);
+            this.imagenRed = itemView.findViewById(R.id.imagen_red_social_id);
+
+        }
+
+        public void cargarRedSocial(RedSocial redSocial) {
+            textViewNombre.setText(redSocial.getNombre());
+            imagenRed.setImageResource(redSocial.getImagenRedSocial());
+        }
+    }
+}
