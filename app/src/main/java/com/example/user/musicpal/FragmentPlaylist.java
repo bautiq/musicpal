77ed5c8a2e93131c738639157cbf7488eabcdd1f
Unknown <bautiquere@gmail.com>
2018-06-04 11:10:47 -0300

    prueba

--- app/src/main/java/com/example/user/musicpal/FragmentPlaylist.java
+++ app/src/main/java/com/example/user/musicpal/FragmentPlaylist.java
@@ -6,13 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentPlaylist extends Fragment {
-
+private TextView textAgregar;
+private ImageView buttonAgregar;
 
 
     @Override
@@ -20,6 +24,20 @@ public class FragmentPlaylist extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_playlist, container, false);
+        textAgregar = view.findViewById(R.id.text_agregar_playlist);
+        buttonAgregar = view.findViewById(R.id.button_agregar_playlist);
+        textAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Click agregar", Toast.LENGTH_SHORT).show();
+            }
+        });
+        buttonAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Click agregar", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 
