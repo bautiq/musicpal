5f8317a1df185f7f72105533b9226dccb944437c
Unknown <bautiquere@gmail.com>
2018-06-15 11:59:28 -0300

    relacion xml y codigo repro

--- app/src/main/java/com/example/user/musicpal/view/FragmentReproductor.java
+++ app/src/main/java/com/example/user/musicpal/view/FragmentReproductor.java
@@ -1,11 +1,14 @@
 package com.example.user.musicpal.view;
 
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.user.musicpal.R;
 
@@ -13,18 +16,26 @@ import com.example.user.musicpal.R;
  * A simple {@link Fragment} subclass.
  */
 public class FragmentReproductor extends Fragment {
-
-
-    public FragmentReproductor() {
-        // Required empty public constructor
-    }
+    private TextView textViewPlayFrom;
+    private TextView textViewPlayType;
+    private ImageView imagen;
+    private ImageView buttonPlayPausa;
+    private ImageView buttonForward;
+    private ImageView buttonBack;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reproductor, container, false);
+        View view = inflater.inflate(R.layout.fragment_reproductor, container, false);
+        textViewPlayFrom = view.findViewById(R.id.text_reproductor_playfrom);
+        textViewPlayType = view.findViewById(R.id.text_reproductor_playtype);
+        imagen = view.findViewById(R.id.imagen_reproductor);
+        buttonPlayPausa = view.findViewById(R.id.button_play_reproductorGrande);
+        buttonForward = view.findViewById(R.id.button_forward_reproductorGrande);
+        buttonBack = view.findViewById(R.id.button_back_reproductorGrande);
+        return view;
     }
 
 }
