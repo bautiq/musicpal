c3e7932691f12f25eddfa8cea1052ac95b1f0acf
Dani <hugodanielmora@gmail.com>
2018-06-26 22:55:51 -0300

    integracion 9

--- app/src/main/java/com/example/user/musicpal/view/FragmentDetalleArtistaPagerAdapter.java
+++ app/src/main/java/com/example/user/musicpal/view/FragmentDetalleArtistaPagerAdapter.java
@@ -1,4 +1,29 @@
 package com.example.user.musicpal.view;
 
-public class FragmentDetalleArtistaPagerAdapter {
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class FragmentDetalleArtistaPagerAdapter extends FragmentStatePagerAdapter {
+    private List<FragmentDetalleArtista> listafragmentDetalleArtistas;
+
+    public FragmentDetalleArtistaPagerAdapter(FragmentManager fm, List<FragmentDetalleArtista> listafragmentDetalleArtistas) {
+        super(fm);
+        this.listafragmentDetalleArtistas = listafragmentDetalleArtistas;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return listafragmentDetalleArtistas.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return listafragmentDetalleArtistas.size();
+    }
+
+
 }
