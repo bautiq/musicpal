e35fac42454ebe5432804306dbbab2cdd41b370f
Unknown <bautiquere@gmail.com>
2018-07-17 09:24:15 -0300

    login activity funcionamiento
    ahora faltaria hacer una funcionalidad que obligue al usuario a registrarse o loguear para usarla

--- app/src/main/java/com/example/user/musicpal/view/LoginActivity.java
+++ app/src/main/java/com/example/user/musicpal/view/LoginActivity.java
@@ -1,18 +1,27 @@
 package com.example.user.musicpal.view;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.user.musicpal.R;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -25,10 +34,17 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authListener;
     private LoginButton loginButton;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+        intent = new Intent(LoginActivity.this, ActivityMain.class);
+        emailEdit = findViewById(R.id.edit_text_email);
+        passEdit = findViewById(R.id.edit_text_password);
+        buttonIngresar = findViewById(R.id.button_ingresar);
+        buttonRegistrarse = findViewById(R.id.button_registrarse);
         callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = findViewById(R.id.login_button);
         loginButton.setReadPermissions();
@@ -36,7 +52,7 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                // App code
+                loguearFirebaseConFacebook(loginResult.getAccessToken());
             }
 
             @Override
@@ -47,6 +63,98 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onError(FacebookException exception) {
                 // App code
+            }
+        });
+
+        buttonIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(emailEdit.getText().toString().equals("") || passEdit.getText().toString().equals("")){
+                    textViewError.setText("Usted no ingreso nada en el campo de email o contraseña");
+                    textViewError.setVisibility(View.VISIBLE);
+                    return;
+                }
+                loguearNativamente(emailEdit.getText().toString(), passEdit.getText().toString());
+            }
+        });
+
+        buttonRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(emailEdit.getText().toString().equals("") || passEdit.getText().toString().equals("")){
+                    textViewError.setText("Usted no ingreso nada en el campo de email o contraseña");
+                    textViewError.setVisibility(View.VISIBLE);
+                    return;
+                }
+                registrarUsuario(emailEdit.getText().toString(), passEdit.getText().toString());
+            }
+        });
+        chequearSiEstaLogueado();
+    }
+
+    private void registrarUsuario(String s, String s1) {
+        buttonIngresar.setVisibility(View.INVISIBLE);
+        buttonRegistrarse.setVisibility(View.INVISIBLE);
+        firebaseAuth.createUserWithEmailAndPassword(s, s1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    buttonIngresar.setVisibility(View.INVISIBLE);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    buttonRegistrarse.setVisibility(View.VISIBLE);
+                    buttonIngresar.setVisibility(View.VISIBLE);
+                    textViewError.setText("Error al registrarse, porfavor asegurese de que escribio un email valido");
+                    textViewError.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    private void chequearSiEstaLogueado() {
+        if (firebaseAuth.getCurrentUser() != null) {
+            startActivity(intent);
+            finish();
+        }
+        if(AccessToken.getCurrentAccessToken() != null){
+            loginButton.setVisibility(View.INVISIBLE);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private void loguearNativamente(String s, String s1) {
+        firebaseAuth.signInWithEmailAndPassword(s, s1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(intent);
+                    finish();
+                } else {
+                    buttonRegistrarse.setVisibility(View.VISIBLE);
+                    buttonIngresar.setVisibility(View.VISIBLE);
+                    textViewError.setText("Error al loguearse, revise si escribio bien el email y password");
+                    textViewError.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+    }
+
+    private void loguearFirebaseConFacebook(AccessToken accessToken) {
+        loginButton.setVisibility(View.INVISIBLE);
+        AuthCredential authCredential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this,
+                            "Autentificacion fallida, Intentelo Nuevamente", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
