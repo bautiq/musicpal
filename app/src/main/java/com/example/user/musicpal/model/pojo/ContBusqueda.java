f4fa2119e65eb864d12a82218ead60fbba891af3
Unknown <marianopotetti@hotmail.com>
2018-07-20 00:46:51 -0300

    trabajando fragment busqueda, celda recycler busqueda y objeto busqueda

---
+++ app/src/main/java/com/example/user/musicpal/model/pojo/ContBusqueda.java
@@ -0,0 +1,16 @@
+package com.example.user.musicpal.model.pojo;
+
+import java.util.List;
+
+public class ContBusqueda {
+
+    private List<Busqueda> listaBusqueda;
+
+    public List<Busqueda> getListaBusqueda() {
+        return listaBusqueda;
+    }
+
+    public void setListaBusqueda(List<Busqueda> listaBusqueda) {
+        this.listaBusqueda = listaBusqueda;
+    }
+}
