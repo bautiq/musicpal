140eb17ecfd4d772a521025ab64511f2f4f1db2c
Unknown <marianopotetti@hotmail.com>
2018-07-23 11:31:23 -0300

    celda busqueda nueva cfoto circulae y buscar con enter

--- app/src/main/java/com/example/user/musicpal/view/FragmentBusqueda.java
+++ app/src/main/java/com/example/user/musicpal/view/FragmentBusqueda.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -19,8 +21,11 @@ import com.example.user.musicpal.controller.ControllerGlobal;
 import com.example.user.musicpal.model.adapters.AdapterFragmentBusqueda;
 import com.example.user.musicpal.model.pojo.Cancion;
 import com.example.user.musicpal.utils.ResultListener;
+import com.example.user.musicpal.utils.SimpleDividerItemDecoration;
 
 import java.util.List;
+
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,13 +62,13 @@ public class FragmentBusqueda extends Fragment implements AdapterFragmentBusqued
         View view = inflater.inflate(R.layout.fragment_busqueda, container, false);
         editTextBusqueda = view.findViewById(R.id.edit_text_busqueda);
         imagenLupaSearch = view.findViewById(R.id.imagen_search_fragment_busqueda);
-
         isLoading = false;
         adapterBusqueda = new AdapterFragmentBusqueda(getActivity(), this);
         linearLayoutManagerBusqueda = new LinearLayoutManager(getActivity(),
                 LinearLayoutManager.VERTICAL,
                 false);
         recyclerViewBusqueda = view.findViewById(R.id.id_recycler_busqueda);
+        recyclerViewBusqueda.addItemDecoration(new SimpleDividerItemDecoration(getActivity()));
 
         recyclerViewBusqueda.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
@@ -83,6 +88,8 @@ public class FragmentBusqueda extends Fragment implements AdapterFragmentBusqued
 
         setAdapterLinear(recyclerViewBusqueda, linearLayoutManagerBusqueda, adapterBusqueda);
         controllerGlobalBusqueda = new ControllerGlobal(getActivity());
+
+        //metodo para esconder el teclado al buscar en editTextBusqueda
         imagenLupaSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
