f7880742685e333d799737b382c9e34f4c2cc70b
Dani <hugodanielmora@gmail.com>
2018-08-01 14:26:19 -0300

    agregado collapsing en listas
    aguardo comentarios

--- app/src/main/java/com/example/user/musicpal/view/FragmentDetallePlaylist.java
+++ app/src/main/java/com/example/user/musicpal/view/FragmentDetallePlaylist.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
